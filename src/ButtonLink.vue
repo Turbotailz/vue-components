f1340a3cd2b983d354b3eab4ed7d9f125b5838a8
Sam Goodger <sam@dcode.com.au>
2019-09-18 16:42:34 +1000

    Add code

---
+++ src/ButtonLink.vue
@@ -0,0 +1,35 @@
+<template>
+    <a :href="href">{{ label }}</a>
+</template>
+
+<script>
+    export default {
+        name: 'button-link',
+
+        props: {
+            label: {
+                required: true,
+                type: String,
+            },
+            href: {
+                required: true,
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    a {
+        background: dodgerblue;
+        color: white;
+        display: inline-block;
+        padding: 1rem 2rem;
+        border: 2px solid dodgerblue;
+
+        &:hover {
+            background: white;
+            color: dodgerblue;
+        }
+    }
+</style>
